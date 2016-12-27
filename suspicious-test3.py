2a23914b9cd9e7fab0858365efdc6e4a1c0b410a
Me-Suspiciuos <fherrera2016@gmail.com>
2016-12-27 19:36:55 +0000

    Using Unpacking arguments, Sets and Dictionary

--- suspicious-test3.py
+++ suspicious-test3.py
@@ -1,4 +1,4 @@
-# How to unpack arguments
+# How to UNPACK arguments
 # Function that calculates the expected age you will live
 def suspicious_calculator(age, fruit_ate, cigs_smoke):
     answer = (100 - age) + (fruit_ate * 3.5) - (cigs_smoke * 2)
@@ -13,3 +13,34 @@ suspicious_calculator(data_list[0], data_list[1], data_list[2])
 # so will take the list and passes each item at the time:
 
 suspicious_calculator(*data_list)
+
+print()
+
+
+# Using SETS: Collections of items which could have any duplicate items
+
+groceries = {'cereal', 'milk', 'cereal', 'beer', 'lotion', 'beer'}
+print(groceries)
+
+# It prints all items but beer which has duplicate values only prints once
+
+if 'milk' in groceries:
+    print('You already have milk')
+else:
+    print('You need to add milk')
+
+print()
+# DICTIONARY: words with associated definitions, in python we call keys and values, For example
+# classmates dictionary
+classmates = {'Ryan': 'Cool chinese guy', 'Natalia': 'Cool english girl', 'Craig': 'Leam Neeson fan'}
+
+print(classmates)
+# Print only one value for a given key
+print(classmates['Natalia'])
+
+print()
+# Looping through all keys
+for k, v in classmates.items():
+    print(k, ' ', v)
+
+
